0b0bce267422f881082545aa29a05c9037b1bfbd
Tim Bueno <timbueno@gmail.com>
2015-08-15 15:04:26 -0400

    Initial Commit

---
+++ TBDirectoryKit/TBDirectoryKit.h
@@ -0,0 +1,19 @@
+//
+//  TBDirectoryKit.h
+//  TBDirectoryKit
+//
+//  Created by Tim Bueno on 2015-08-15.
+//  Copyright © 2015 Tim Bueno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBDirectoryKit.
+FOUNDATION_EXPORT double TBDirectoryKitVersionNumber;
+
+//! Project version string for TBDirectoryKit.
+FOUNDATION_EXPORT const unsigned char TBDirectoryKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBDirectoryKit/PublicHeader.h>
+
+
