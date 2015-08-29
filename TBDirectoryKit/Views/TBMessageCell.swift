683d6fbb8317da25ac19d9daec104239f0e46654
Tim Bueno <timbueno@gmail.com>
2015-08-29 14:21:38 -0400

    Made class public - duh.

--- TBDirectoryKit/Views/TBMessageCell.swift
+++ TBDirectoryKit/Views/TBMessageCell.swift
@@ -8,24 +8,24 @@
 
 import UIKit
 
-class TBMessageCell: UITableViewCell {
+public class TBMessageCell: UITableViewCell {
     
     
-    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet public weak var messageLabel: UILabel!
     
 
-    override func awakeFromNib() {
+    override public func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
+    override public func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
     
-    func setMessage(message: String) {
+    public func setMessage(message: String) {
         messageLabel.text = message
     }
 
